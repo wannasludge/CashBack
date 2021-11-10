4db605bae90ad120ec01d29c9bb5dde3dec00dd3
Alexander mokhov <wannasludge@yandex.ru>
2021-11-10 23:42:29 +0300

    Use JUnit4

--- src/test/java/ru/netology/Service/CashBackServiceTest.java
+++ src/test/java/ru/netology/Service/CashBackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.Service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashBackServiceTest {
     @Test
@@ -10,7 +10,7 @@ public class CashBackServiceTest {
         CashBackService service = new CashBackService();
         int actual = service.remain(400);
         int expected = 600;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -18,20 +18,22 @@ public class CashBackServiceTest {
         CashBackService service = new CashBackService();
         int actual = service.remain(1600);
         int expected = 400;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldCheckCashbackAmountEqual1000() {
         CashBackService service = new CashBackService();
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void ShouldCheckCashbackAmountEqualZero() {
         CashBackService service = new CashBackService();
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
